ae85aa638c5b62e823ae95266277f15cf9a01657
pietrostraneo <straneo_p@yahoo.it>
2024-02-14 16:03:25 +0100

    Recovered the contents of the db inside the MovieController file

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Movie;
+
 use Illuminate\Http\Request;
 
 class MovieController extends Controller
 {
-    //
+    public function index()
+    {
+        $movies = Movie::all();
+
+        return view('movies', compact('movies'));
+    }
 }
